a36d45f80825d22862b5506faed329257a554e2b
Yura <yuriy.petrashenko7@gmail.com>
2020-04-06 12:59:34 +0300

    Add task4

---
+++ src/main/java/com/yura/stepic/course/Task4.java
@@ -0,0 +1,21 @@
+package com.yura.stepic.course;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(gcd(a, b));
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
